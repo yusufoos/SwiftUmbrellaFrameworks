dd815eab0d89909c49060e96b23aa4345a1ea7ce
Yusuf Sobh <ysobh@linkedin.com>
2022-04-06 12:38:04 -0700

    Initial Commit

---
+++ SDKModularization/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SDKModularization
+//
+//  Created by Yusuf Sobh on 4/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
